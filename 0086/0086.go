d13bfa827992766bdb5f1316d499342d3f28dc01
ronething <axingfly@gmail.com>
2019-11-13 00:22:52 +0800

    feat: 0086

--- 0086/0086.go
+++ 0086/0086.go
@@ -1,5 +1,94 @@
 package main
 
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func partition(head *ListNode, x int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	res := &ListNode{}
+	temp := head
+	for temp.Next != nil {
+		temp = temp.Next
+	}
+	oldLast, last := temp, temp
+	temp = res
+	for head != nil {
+		if head == last {
+			if head.Next == nil {
+				temp.Next = head
+				temp = temp.Next
+				head = head.Next
+			} else {
+				if head.Val >= x {
+					temp.Next = head.Next
+					//temp = temp.Next
+					oldLast.Next = head
+					oldLast = oldLast.Next
+					oldLast.Next = nil
+					head = temp.Next
+				} else {
+					temp.Next = head
+					temp = temp.Next
+					head = head.Next
+				}
+			}
+
+			break
+		}
+		if head.Val >= x {
+			temp.Next = head.Next
+			//temp = temp.Next
+			oldLast.Next = head
+			oldLast = oldLast.Next
+			oldLast.Next = nil
+			head = temp.Next
+		} else {
+			temp.Next = head
+			temp = temp.Next
+			head = head.Next
+		}
+	}
+
+	return res.Next
+
+}
+
 func main() {
-	$END$
+	node := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val:  2,
+							Next: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+	//node := &ListNode{
+	//	Val: 1,
+	//	Next: &ListNode{
+	//		Val:  2,
+	//		Next: nil,
+	//	},
+	//}
+	res := partition(node, 3)
+	for res != nil {
+		fmt.Println(res.Val)
+		res = res.Next
+	}
 }
--- 0086/another/0086.go
+++ 0086/another/0086.go
@@ -1,5 +1,61 @@
 package main
 
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func partition(head *ListNode, x int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	before := &ListNode{}
+	less := before
+	after := &ListNode{}
+	great := after
+	for head != nil {
+		if head.Val < x {
+			less.Next = head
+			less = less.Next
+		} else {
+			great.Next = head
+			great = great.Next
+		}
+		head = head.Next
+	}
+	great.Next = nil // 避免环形链表
+	less.Next = after.Next
+
+	return before.Next
+
+}
+
 func main() {
-	$END$
+	node := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val:  2,
+							Next: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+	res := partition(node, 3)
+	for res != nil {
+		fmt.Println(res.Val)
+		res = res.Next
+	}
 }
