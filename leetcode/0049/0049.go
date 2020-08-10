83f9fa9aa8fed40590183db42ad6e220bb4df2ca
ronething <axingfly@gmail.com>
2020-08-11 00:43:23 +0800

    feat: 49

---
+++ leetcode/0049/0049.go
@@ -0,0 +1,10 @@
+package _049
+
+func groupAnagrams(strs []string) [][]string {
+	var a [][]string
+	for i := 0; i < len(strs); i++ {
+		for j := 0; j < len(strs[i]); j++ {
+
+		}
+	}
+}
---
+++ leetcode/0049/java/lc49.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class lc49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new lc49().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+
+}
