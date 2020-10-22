d6dccb5d111ed8daf8ebc8d39c3645f110d99d33
ronething <axingfly@gmail.com>
2020-10-23 01:19:22 +0800

    [feat] 763. Partition Labels

--- leetcode/0763/763. Partition Labels.go
+++ leetcode/0763/763. Partition Labels.go
@@ -1,1 +1,26 @@
 package _763
+
+func partitionLabels(S string) []int {
+	if len(S) == 0 {
+		return nil
+	}
+	//计算所有字符的最后一次出现的位置
+	m := make(map[byte]int)
+	for i := 0; i < len(S); i++ {
+		m[S[i]] = i
+	}
+	//双指针
+	start := 0
+	end := 0
+	var res []int
+	for i := 0; i < len(S); i++ {
+		if m[S[i]] > end {
+			end = m[S[i]]
+		}
+		if i == end {
+			res = append(res, end-start+1)
+			start = end + 1
+		}
+	}
+	return res
+}
--- leetcode/0763/763. Partition Labels_test.go
+++ leetcode/0763/763. Partition Labels_test.go
@@ -1,1 +1,15 @@
 package _763
+
+import (
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func Test_partitionLabels(t *testing.T) {
+	assert.Equal(
+		t,
+		[]int{9, 7, 8},
+		partitionLabels("ababcbacadefegdehijhklij"),
+	)
+}
