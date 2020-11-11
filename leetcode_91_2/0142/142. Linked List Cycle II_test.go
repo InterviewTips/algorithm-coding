60a253a2d51b4782b6279c75a2d02575ba34e518
ronething <axingfly@gmail.com>
2020-11-11 22:43:24 +0800

    [feat] 142. Linked List Cycle II

---
+++ leetcode_91_2/0142/142. Linked List Cycle II_test.go
@@ -0,0 +1,38 @@
+package _142
+
+import "github.com/InterviewTips/algorithm-coding/guns"
+
+type ListNode = guns.ListNode
+
+func detectCycle1(head *ListNode) *ListNode {
+	arr := make(map[*ListNode]int)
+	for head != nil {
+		_, ok := arr[head]
+		if ok {
+			return head
+		}
+		arr[head] = 1
+		head = head.Next
+	}
+	return nil
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for slow != p {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
