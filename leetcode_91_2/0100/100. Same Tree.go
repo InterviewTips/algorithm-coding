a3dd262e12fccd55874e32b27a2be91ead723333
ronething <axingfly@gmail.com>
2020-11-14 11:05:30 +0800

    [feat] 100. Same Tree

---
+++ leetcode_91_2/0100/100. Same Tree.go
@@ -0,0 +1,15 @@
+package _100
+
+import "github.com/InterviewTips/algorithm-coding/guns"
+
+type TreeNode = guns.TreeNode
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
