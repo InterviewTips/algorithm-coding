28efef4136bc11dfe5c66823d509dd1d88ad87fa
ashing <axingfly@gmail.com>
2021-07-12 23:57:16 +0800

    [feat] reverseList

---
+++ leetcode/0206/go/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/InterviewTips/algorithm-coding/guns"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode = guns.ListNode
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		cur = next
+		prev = cur // 位移
+	}
+
+	return prev
+}
+
+//func reverseList(head *ListNode) *ListNode {
+//	if head == nil || head.Next == nil {
+//		return head
+//	}
+//
+//	// 双指针
+//	pre := &ListNode{Val: head.Val, Next: nil}
+//	for head != nil {
+//		cur := head
+//		head = head.Next
+//		pre = head
+//		pre.Next = cur
+//	}
+//	return pre
+//
+//}
