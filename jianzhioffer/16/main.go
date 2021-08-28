19adec846628eec8fe97588ab45ab28168daeb33
ronething <axingfly@gmail.com>
2021-08-28 17:10:22 +0800

    [chore] comment

--- jianzhioffer/16/main.go
+++ jianzhioffer/16/main.go
@@ -5,10 +5,10 @@ func myPow(base float64, exponent int) float64 {
 		return 0
 	}
 
-	absExponent := getAbsExponent(exponent)
+	absExponent := getAbsExponent(exponent) // 获取指数的绝对值
 
 	res := getResult(base, absExponent)
-	if exponent < 0 {
+	if exponent < 0 { // 如果指数是负数，则需要取倒数
 		res = 1.0 / res
 	}
 
@@ -24,10 +24,10 @@ func getResult(base float64, exponent int) float64 { // 递归
 		return base
 	}
 
-	result := getResult(base, exponent>>1)
+	result := getResult(base, exponent>>1) // 右移得到 exponent/2
 	result *= result
 
-	if exponent&0x1 == 1 {
+	if exponent&0x1 == 1 { // 位运算判断是否是奇数
 		result *= base
 	}
 
