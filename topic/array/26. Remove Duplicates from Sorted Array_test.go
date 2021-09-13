82e56cb653a6434b20021c7c8fbd265ce7940379
ashing <axingfly@gmail.com>
2021-09-14 01:01:02 +0800

    [feat] 快慢指针

---
+++ topic/array/26. Remove Duplicates from Sorted Array_test.go
@@ -0,0 +1,32 @@
+package array
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "one",
+			args: args{nums: []int{1, 1, 2}},
+			want: 2,
+		},
+		{
+			name: "two",
+			args: args{nums: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
