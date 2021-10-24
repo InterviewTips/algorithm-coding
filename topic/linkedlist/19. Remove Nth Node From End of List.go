6bdfba9595c5ba4f2599655fd731c567d837203a
ashing <axingfly@gmail.com>
2021-10-24 23:52:45 +0800

    [feat] 19. Remove Nth Node From End of List.go

---
+++ topic/linkedlist/19. Remove Nth Node From End of List.go
@@ -0,0 +1,23 @@
+package linkedlist
+
+// 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+//
+//进阶：你能尝试使用一趟扫描实现吗？
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+	slow := dummyHead
+	fast := head // 这里指向 head fast 就只需要走 n-1 步，如果指向 dummyHead 就需要走 n 步
+	for i := 0; i < n-1; i++ {
+		fast = fast.Next
+	}
+
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return dummyHead.Next
+}
---
+++ topic/linkedlist/19. Remove Nth Node From End of List_test.go
@@ -0,0 +1,50 @@
+package linkedlist
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_removeNthFromEnd(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "one",
+			args: args{
+				head: NewLinkedList([]int{1, 2, 3, 4, 5}),
+				n:    2,
+			},
+			want: NewLinkedList([]int{1, 2, 3, 5}),
+		},
+		{
+			name: "two",
+			args: args{
+				head: NewLinkedList([]int{1}),
+				n:    1,
+			},
+			want: NewLinkedList([]int{}),
+		},
+		{
+			name: "three",
+			args: args{
+				head: NewLinkedList([]int{1, 2}),
+				n:    1,
+			},
+			want: NewLinkedList([]int{1}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
