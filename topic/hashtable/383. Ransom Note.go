14cc6ce0441ef598731e83c8f08083187b1c8fbf
ashing <axingfly@gmail.com>
2021-11-05 22:46:26 +0800

    [feat] 383. Ransom Note

--- topic/hashtable/383. Ransom Note.go
+++ topic/hashtable/383. Ransom Note.go
@@ -1,5 +1,20 @@
 package hashtable
 
 func canConstruct(ransomNote string, magazine string) bool {
+	// 构建 map
+	magazineMap := make(map[uint8]int)
+	for i := 0; i < len(magazine); i++ {
+		m := magazine[i]
+		magazineMap[m]++
+	}
 
+	for i := 0; i < len(ransomNote); i++ {
+		r := ransomNote[i]
+		magazineMap[r]--
+		if magazineMap[r] < 0 {
+			return false
+		}
+	}
+
+	return true
 }
