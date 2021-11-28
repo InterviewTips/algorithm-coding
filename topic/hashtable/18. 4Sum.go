d7e14f3d50acdc04cb6ed0c7462ec6dd7ace4e34
ashing <axingfly@gmail.com>
2021-11-29 00:27:15 +0800

    [feat] 18. 4Sum

---
+++ topic/hashtable/18. 4Sum.go
@@ -0,0 +1,57 @@
+package hashtable
+
+import (
+	"sort"
+)
+
+func fourSum(nums []int, target int) [][]int {
+	res := make([][]int, 0)
+	if len(nums) < 4 {
+		return res
+	}
+	// 排序先
+	sort.SliceStable(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	for i := 0; i <= len(nums)-4; i++ {
+		num1 := nums[i]
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j := i + 1; j < len(nums); j++ { // j < len(nums) 而不是 j <= len(nums) - 4
+			num2 := nums[j]
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			needCalc := target - (num1 + num2)
+			left := j + 1
+			right := len(nums) - 1
+			//log.Println(nums[left], nus[right])
+			for left < right {
+				if nums[left]+nums[right] == needCalc {
+					res = append(res, []int{num1, num2, nums[left], nums[right]})
+					for left < right && nums[left] == nums[left+1] {
+						left++
+					}
+					for left < right && nums[right] == nums[right-1] {
+						right--
+					}
+					left++
+					right--
+				} else if nums[left]+nums[right] < needCalc {
+					for left < right && nums[left] == nums[left+1] {
+						left++
+					}
+					left++
+				} else if nums[left]+nums[right] > needCalc {
+					for left < right && nums[right] == nums[right-1] {
+						right--
+					}
+					right--
+				}
+			}
+		}
+	}
+
+	return res
+}
---
+++ topic/hashtable/18. 4Sum_test.go
@@ -0,0 +1,58 @@
+package hashtable
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fourSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "one",
+			args: args{
+				nums:   []int{2, 2, 2, 2, 2},
+				target: 8,
+			},
+			want: [][]int{
+				{2, 2, 2, 2},
+			},
+		},
+		{
+			name: "two",
+			args: args{
+				nums:   []int{1, 0, -1, 0, -2, 2},
+				target: 0,
+			},
+			want: [][]int{
+				{-2, -1, 1, 2},
+				{-2, 0, 0, 2},
+				{-1, 0, 0, 1},
+			},
+		},
+		{
+			name: "zero",
+			args: args{
+				nums:   []int{0, 0, 0, 0},
+				target: 0,
+			},
+			want: [][]int{
+				{0, 0, 0, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fourSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fourSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
