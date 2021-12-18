187a2dee2596e83e53a978faba7d1616d161bf94
ashing <axingfly@gmail.com>
2021-12-18 14:00:45 +0800

    [feat] 151. Reverse Words in a String another

--- topic/string/151. Reverse Words in a String.go
+++ topic/string/151. Reverse Words in a String.go
@@ -23,3 +23,54 @@ func reverseWords(s string) string {
 	// fmt.Println(b)
 	return c
 }
+
+// 分三步
+// 1、移除多余空格 快慢指针
+// 2、整个字符串反转
+// 3、单词反转
+func reverseWords1(s string) string {
+	noSpace := removeExtraSpace(s)
+	reverseBytes := reverseSingleStr(noSpace)
+	res := reverseSingleWord(reverseBytes)
+	return res
+}
+
+func removeExtraSpace(s string) []byte {
+	slowIndex := 0
+	fastIndex := 0
+	res := make([]byte, len(s))
+	// 移除前面的空格
+	for len(s) > 0 && fastIndex < len(s) && s[fastIndex] == ' ' {
+		fastIndex++
+	}
+	// 移除中间的多余空格
+	for ; fastIndex < len(s); fastIndex++ {
+		if fastIndex-1 > 0 && s[fastIndex-1] == s[fastIndex] && s[fastIndex] == ' ' {
+			continue
+		} else {
+			res[slowIndex] = s[fastIndex]
+			slowIndex++
+		}
+	}
+	// 移除 res 后面的多余空格
+	// 此时 slowIndex 是数组的长度
+	if slowIndex-1 > 0 && res[slowIndex-1] == ' ' { // 判断一下最后一个字符是不是空格 注意这里是 res 的 [slowIndex-1]
+		return res[:slowIndex-1]
+	} else {
+		return res[:slowIndex]
+	}
+}
+
+func reverseSingleWord(reverseBytes []byte) string {
+	res := make([]string, 0)
+	for i := 0; i < len(reverseBytes); i++ {
+		j := i
+		for j < len(reverseBytes) && reverseBytes[j] != ' ' {
+			j++
+		}
+		res = append(res, string(reverseSingleStr(reverseBytes[i:j])))
+		i = j // i 变为 j 这个时候 j 是空格的索引
+	}
+
+	return strings.Join(res, " ")
+}
--- topic/string/151. Reverse Words in a String_test.go
+++ topic/string/151. Reverse Words in a String_test.go
@@ -1,6 +1,9 @@
 package string
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_reverseWords(t *testing.T) {
 	type args struct {
@@ -23,6 +26,84 @@ func Test_reverseWords(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := reverseWords(tt.args.s); got != tt.want {
 				t.Errorf("reverseWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseWords1(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "one",
+			args: args{
+				s: " hello   world ",
+			},
+			want: "world hello",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseWords1(tt.args.s); got != tt.want {
+				t.Errorf("reverseWords1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_removeExtraSpace(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "one",
+			args: args{
+				s: " hello   world ",
+			},
+			want: []byte("hello world"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeExtraSpace(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeExtraSpace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseSingleWord(t *testing.T) {
+	type args struct {
+		reverseBytes []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "one",
+			args: args{
+				reverseBytes: []byte("olleh drow"),
+			},
+			want: "hello word",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseSingleWord(tt.args.reverseBytes); got != tt.want {
+				t.Errorf("reverseSingleWord() = %v, want %v", got, tt.want)
 			}
 		})
 	}
