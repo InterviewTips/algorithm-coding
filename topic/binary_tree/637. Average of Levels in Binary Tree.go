96a86089653a2bbe1e41dd59454b9556baeb2d2c
ashing <axingfly@gmail.com>
2022-02-12 15:57:51 +0800

    [feat] 637. Average of Levels in Binary Tree

---
+++ topic/binary_tree/637. Average of Levels in Binary Tree.go
@@ -0,0 +1,35 @@
+package binary_tree
+
+// 层次遍历的变种 也可以使用深度搜索
+func averageOfLevels(root *TreeNode) []float64 {
+	res := make([]float64, 0)
+	if root == nil {
+		return res
+	}
+	treeQueue := make([]*TreeNode, 0)
+	// push root to queue
+	treeQueue = append(treeQueue, root)
+	for len(treeQueue) != 0 {
+		size := len(treeQueue)
+		var levelSum float64
+		for i := 0; i < size; i++ {
+			// pop_front
+			node := treeQueue[0]
+			treeQueue = treeQueue[1:]
+			if node.Left != nil {
+				treeQueue = append(treeQueue, node.Left)
+			}
+			if node.Right != nil {
+				treeQueue = append(treeQueue, node.Right)
+			}
+			levelSum += float64(node.Val)
+		}
+		levelAverage := levelSum / float64(size)
+		// 保留 .5f 官方判题样例貌似没有对此进行要求
+		//value, _ := strconv.ParseFloat(fmt.Sprintf("%.5f", levelAverage), 64)
+		//res = append(res, value)
+		res = append(res, levelAverage)
+	}
+
+	return res
+}
---
+++ topic/binary_tree/637. Average of Levels in Binary Tree_test.go
@@ -0,0 +1,32 @@
+package binary_tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_averageOfLevels(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []float64
+	}{
+		{
+			name: "one",
+			args: args{
+				root: CreateBinaryTree([]int{3, 9, 20, Null, Null, 15, 7}),
+			},
+			want: []float64{3.00000, 14.50000, 11.00000},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := averageOfLevels(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("averageOfLevels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
