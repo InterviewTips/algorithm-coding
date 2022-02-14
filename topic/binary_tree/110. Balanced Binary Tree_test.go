1e2dc033c7c636eb4950db457800863de4031474
ashing <axingfly@gmail.com>
2022-02-15 00:02:56 +0800

    [feat] 110. Balanced Binary Tree

---
+++ topic/binary_tree/110. Balanced Binary Tree_test.go
@@ -0,0 +1,29 @@
+package binary_tree
+
+import "testing"
+
+func Test_isBalanced(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "one",
+			args: args{
+				root: CreateBinaryTree([]int{3, 9, 20, Null, Null, 15, 7}),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isBalanced(tt.args.root); got != tt.want {
+				t.Errorf("isBalanced() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
