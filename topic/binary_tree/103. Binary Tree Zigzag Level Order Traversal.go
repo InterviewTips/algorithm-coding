74e8dc7cf821752cb3a93f4d4537fff91b440bc1
ashing <axingfly@gmail.com>
2022-02-18 00:35:12 +0800

    [feat] 103. Binary Tree Zigzag Level Order Traversal

---
+++ topic/binary_tree/103. Binary Tree Zigzag Level Order Traversal.go
@@ -0,0 +1,46 @@
+package binary_tree
+
+// 层次遍历变种
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	res := make([][]int, 0)
+	if root == nil {
+		return res
+	}
+	treeQueue := make([]*TreeNode, 0)
+	// push root
+	treeQueue = append(treeQueue, root)
+	positive := true
+	for len(treeQueue) != 0 {
+		levelRes := make([]int, 0) // 其中一层 res
+		size := len(treeQueue)     // 先取出当前 queue 长度 其实就是需要将上一层的节点都取出来
+		// 不能直接使用 len(treeQueue) 的原因在于 treeQueue 的长度在下面会有变动
+		for i := 0; i < size; i++ {
+			// pop_front
+			node := treeQueue[0]
+			treeQueue = treeQueue[1:]
+			// 加入 levelRes
+			levelRes = append(levelRes, node.Val)
+			// push to queue
+			if node.Left != nil {
+				treeQueue = append(treeQueue, node.Left)
+			}
+			if node.Right != nil {
+				treeQueue = append(treeQueue, node.Right)
+			}
+		}
+		if !positive { // 逆向
+			reverseLevelRes(&levelRes)
+		}
+		positive = !positive
+		// levelRes append to res
+		res = append(res, levelRes)
+	}
+
+	return res
+}
+
+func reverseLevelRes(res *[]int) {
+	for i := 0; i < len(*res)/2; i++ {
+		(*res)[i], (*res)[len(*res)-i-1] = (*res)[len(*res)-i-1], (*res)[i]
+	}
+}
---
+++ topic/binary_tree/103. Binary Tree Zigzag Level Order Traversal_test.go
@@ -0,0 +1,34 @@
+package binary_tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_zigzagLevelOrder(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "one",
+			args: args{
+				root: CreateBinaryTree([]int{3, 9, 20, Null, Null, 15, 7}),
+			},
+			want: [][]int{
+				{3}, {20, 9}, {15, 7},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := zigzagLevelOrder(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("zigzagLevelOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
