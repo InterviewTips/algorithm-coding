59790c8f98e3e1f38f6b8ed05de6b1c64f9a9382
ashing <axingfly@gmail.com>
2022-02-18 21:01:27 +0800

    [feat] 450. Delete Node in a BST

--- topic/binary_tree/450. Delete Node in a BST.go
+++ topic/binary_tree/450. Delete Node in a BST.go
@@ -1,6 +1,33 @@
 package binary_tree
 
 func deleteNode(root *TreeNode, key int) *TreeNode {
-	// todo
-	return nil
+	if root == nil {
+		return nil
+	}
+
+	if key < root.Val {
+		root.Left = deleteNode(root.Left, key)
+	}
+
+	if key > root.Val {
+		root.Right = deleteNode(root.Right, key)
+	}
+
+	if key == root.Val {
+		left := root.Left
+		right := root.Right
+		if right != nil {
+			// 找到 node.Left == nil 的节点
+			node := right
+			for node.Left != nil {
+				node = node.Left
+			}
+			node.Left = left
+			return right
+		} else {
+			return left
+		}
+	}
+
+	return root
 }
