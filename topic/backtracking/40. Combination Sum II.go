55a6e38a3c6fd307bfc1b5415530fa5a7c0a5136
ashing <axingfly@gmail.com>
2022-02-19 16:54:44 +0800

    [feat] 40. Combination Sum II

--- topic/backtracking/40. Combination Sum II.go
+++ topic/backtracking/40. Combination Sum II.go
@@ -45,3 +45,43 @@ func combinationSum2(candidates []int, target int) [][]int {
 
 	return res
 }
+
+// var combinationSum2 = combinationSum2Another
+func combinationSum2Another(candidates []int, target int) [][]int {
+	res := make([][]int, 0)
+	path := make([]int, 0)
+	// 排序 从小到大
+	sort.SliceStable(candidates, func(i, j int) bool {
+		return candidates[i] < candidates[j]
+	})
+	var backtracking func(sum int, index int)
+	backtracking = func(sum int, index int) {
+		if sum > target {
+			return
+		}
+
+		if sum == target {
+			value := make([]int, len(path))
+			copy(value, path)
+			res = append(res, value)
+			return
+		}
+
+		for i := index; i < len(candidates) && sum+candidates[i] <= target; i++ {
+			// i > index 表示到了下一个树枝
+			if i > index && candidates[i] == candidates[i-1] {
+				// 表示同一个树层 前面已经选取过
+				continue
+			}
+			path = append(path, candidates[i])
+			backtracking(sum+candidates[i], i+1)
+			path = path[:len(path)-1]
+		}
+	}
+
+	backtracking(0, 0)
+
+	log.Println(res)
+
+	return res
+}
--- topic/backtracking/40. Combination Sum II_test.go
+++ topic/backtracking/40. Combination Sum II_test.go
@@ -48,3 +48,47 @@ func Test_combinationSum2(t *testing.T) {
 		})
 	}
 }
+
+func Test_combinationSum2Another(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "one",
+			args: args{
+				candidates: []int{10, 1, 2, 7, 6, 1, 5},
+				target:     8,
+			},
+			want: [][]int{
+				{1, 1, 6},
+				{1, 2, 5},
+				{1, 7},
+				{2, 6},
+			},
+		},
+		{
+			name: "two",
+			args: args{
+				candidates: []int{2, 5, 2, 1, 2},
+				target:     5,
+			},
+			want: [][]int{
+				{1, 2, 2},
+				{5},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSum2Another(tt.args.candidates, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSum2Another() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
