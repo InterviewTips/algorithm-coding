765c2347b404042fc7743d4a28fc11319ca4d112
ashing <axingfly@gmail.com>
2022-02-19 16:49:14 +0800

    [feat] 40. Combination Sum II

---
+++ topic/backtracking/40. Combination Sum II.go
@@ -0,0 +1,47 @@
+package backtracking
+
+import (
+	"log"
+	"sort"
+)
+
+func combinationSum2(candidates []int, target int) [][]int {
+	res := make([][]int, 0)
+	path := make([]int, 0)
+	used := make([]bool, len(candidates))
+	// 排序 从小到大
+	sort.SliceStable(candidates, func(i, j int) bool {
+		return candidates[i] < candidates[j]
+	})
+	var backtracking func(sum int, index int)
+	backtracking = func(sum int, index int) {
+		if sum > target {
+			return
+		}
+
+		if sum == target {
+			value := make([]int, len(path))
+			copy(value, path)
+			res = append(res, value)
+			return
+		}
+
+		for i := index; i < len(candidates) && sum+candidates[i] <= target; i++ {
+			if i > 0 && candidates[i] == candidates[i-1] && used[i-1] == false {
+				// 表示同一个树层 前面已经选取过
+				continue
+			}
+			used[i] = true
+			path = append(path, candidates[i])
+			backtracking(sum+candidates[i], i+1)
+			used[i] = false
+			path = path[:len(path)-1]
+		}
+	}
+
+	backtracking(0, 0)
+
+	log.Println(res)
+
+	return res
+}
---
+++ topic/backtracking/40. Combination Sum II_test.go
@@ -0,0 +1,50 @@
+package backtracking
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_combinationSum2(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "one",
+			args: args{
+				candidates: []int{10, 1, 2, 7, 6, 1, 5},
+				target:     8,
+			},
+			want: [][]int{
+				{1, 1, 6},
+				{1, 2, 5},
+				{1, 7},
+				{2, 6},
+			},
+		},
+		{
+			name: "two",
+			args: args{
+				candidates: []int{2, 5, 2, 1, 2},
+				target:     5,
+			},
+			want: [][]int{
+				{1, 2, 2},
+				{5},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSum2(tt.args.candidates, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
