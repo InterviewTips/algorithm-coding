9073af7d1068dfd53405c4e71b7abff7734cb217
ashing <axingfly@gmail.com>
2022-02-26 13:42:32 +0800

    [chore] udpate comment

--- topic/dp/746. Min Cost Climbing Stairs.go
+++ topic/dp/746. Min Cost Climbing Stairs.go
@@ -8,7 +8,8 @@ func minCostClimbingStairs(cost []int) int {
 
 	// 注意：楼顶是比最后一个元素还后面
 	// res[i] 表示爬到第 i 层需要花费的 sum
-	// res[i] = getMin(res[i-1]+cost[i-1], res[i-2]+cost[i-2])
+	// res[0] = 0, res[1] = 0 表示如果要到达前两层不需要花费 cost
+	// res[i] = getMin(res[i-1]+cost[i-1], res[i-2]+cost[i-2]) (i >= 2)
 	// 爬到 i-1 需要花费的 sum + cost[i-1] 再爬一层可以爬到 i
 	// 爬到 i-2 需要花费的 sum + cost[i-2] 再爬两层可以爬到 i
 	// 取出两者最小值
