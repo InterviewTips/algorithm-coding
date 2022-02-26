f259e8533ba9124634d87c18107b2bc87f715bb2
ashing <axingfly@gmail.com>
2022-02-26 16:04:45 +0800

    [chore] update comment

--- topic/dp/343. Integer Break.go
+++ topic/dp/343. Integer Break.go
@@ -1,5 +1,7 @@
 package dp
 
+import "log"
+
 // var integerBreak = dpIntegerBreak
 func dpIntegerBreak(n int) int {
 	if n == 1 {
@@ -11,12 +13,20 @@ func dpIntegerBreak(n int) int {
 		memo[i] = -1
 	}
 
-	memo[1] = 1
-	for i := 2; i <= n; i++ {
+	memo[2] = 1 // 其实只需要初始化 索引 2 即可 索引 0/1 没有意义
+	for i := 3; i <= n; i++ {
 		// 计算 integerBreak(i)
 		for j := 1; j <= i-1; j++ {
 			// j+(i-j)
+			log.Printf("i: %v, memo[i]: %v\n", i, memo[i])
+			//memo[i] = getMax3(memo[i], j*(i-j), j*memo[i-j])
 			memo[i] = getMax3(memo[i], j*(i-j), j*memo[i-j])
+			// 只能拆 一个 至于拆 j 还是 i-j 随意
+			//memo[i] = getMax3(memo[i], (i-j) * j, (i-j) * memo[j])
+			// memo[i] 需要放进来进行比较 getMax3，因为可能会进入多次 即可能拆分多次 例如 1 和 3, 2 和 2
+			// 2022/02/26 15:55:39 i: 4, memo[i]: -1
+			// 2022/02/26 15:55:39 i: 4, memo[i]: 3
+			// 2022/02/26 15:55:39 i: 4, memo[i]: 4
 		}
 	}
 
