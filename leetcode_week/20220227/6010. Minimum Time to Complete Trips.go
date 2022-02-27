0d4ec1697ce45b8c1b35b0d7cbfcc48ccd91be4e
ashing <axingfly@gmail.com>
2022-02-27 11:30:44 +0800

    [err] timeout

--- leetcode_week/20220227/6010. Minimum Time to Complete Trips.go
+++ leetcode_week/20220227/6010. Minimum Time to Complete Trips.go
@@ -6,6 +6,16 @@ func minimumTime(time []int, totalTrips int) int64 {
 	sort.SliceStable(time, func(i, j int) bool {
 		return time[i] < time[j]
 	})
+	lcmValue := time[0]
+	for i := 1; i < len(time); i++ {
+		lcmValue = lcm(time[i], lcmValue)
+	}
+	quanValue := 0
+	for i := 0; i < len(time); i++ {
+		quanValue += lcmValue / time[i]
+	}
+	quan := totalTrips / quanValue
+	left := totalTrips % quanValue
 	t := time[0]
 	count := 0
 	for {
@@ -13,7 +23,7 @@ func minimumTime(time []int, totalTrips int) int64 {
 		for i := 0; i < len(time); i++ {
 			//log.Println("t is",t, "time is", time[i]
 			count += t / time[i]
-			if count >= totalTrips {
+			if count >= left {
 				goto end
 			}
 		}
@@ -21,5 +31,18 @@ func minimumTime(time []int, totalTrips int) int64 {
 	}
 end:
 
-	return int64(t)
+	return int64(quan*lcmValue + t)
+}
+
+func lcm(x, y int) int {
+	return x * y / gcd(x, y)
+}
+
+func gcd(x, y int) int {
+	tmp := x % y
+	if tmp > 0 {
+		return gcd(y, tmp)
+	} else {
+		return y
+	}
 }
