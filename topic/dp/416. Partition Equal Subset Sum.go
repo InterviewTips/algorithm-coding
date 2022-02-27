225377f832a96e228ab4966c7f6854ea31ff8fd7
ashing <axingfly@gmail.com>
2022-02-27 19:19:35 +0800

    [chore] update comment

--- topic/dp/416. Partition Equal Subset Sum.go
+++ topic/dp/416. Partition Equal Subset Sum.go
@@ -12,6 +12,8 @@ func canPartition(nums []int) bool {
 
 	bagWeight := sum / 2
 
+	// res[j]表示 背包总容量是 j，最大可以凑成 j 的子集总和为 dp[j]。
+	// weight[i] 和 value[i] 在这道题目中相等
 	res := make([]int, bagWeight+1)
 	res[0] = 0
 	for i := 0; i < len(nums); i++ {
