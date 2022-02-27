62c33e77805de45b14bc5e7233aef1383c1441be
ashing <axingfly@gmail.com>
2022-02-27 13:17:42 +0800

    [fix] minimumTime binary search

--- leetcode_week/20220227/6010. Minimum Time to Complete Trips.go
+++ leetcode_week/20220227/6010. Minimum Time to Complete Trips.go
@@ -1,48 +1,30 @@
 package _0220227
 
-import "sort"
-
 func minimumTime(time []int, totalTrips int) int64 {
-	sort.SliceStable(time, func(i, j int) bool {
-		return time[i] < time[j]
-	})
-	lcmValue := time[0]
-	for i := 1; i < len(time); i++ {
-		lcmValue = lcm(time[i], lcmValue)
-	}
-	quanValue := 0
-	for i := 0; i < len(time); i++ {
-		quanValue += lcmValue / time[i]
-	}
-	quan := totalTrips / quanValue
-	left := totalTrips % quanValue
-	t := time[0]
-	count := 0
-	for {
-		count = 0
-		for i := 0; i < len(time); i++ {
-			//log.Println("t is",t, "time is", time[i]
-			count += t / time[i]
-			if count >= left {
-				goto end
-			}
+
+	// 二分查找
+	left := 1
+	right := totalTrips * 1e7 // 上界
+
+	// [left, right)
+	for left < right {
+		mid := left + (right-left)/2
+		count := calcTrips(mid, time)
+		if count < totalTrips {
+			left = mid + 1
+		} else {
+			right = mid
 		}
-		t += time[0]
 	}
-end:
 
-	return int64(quan*lcmValue + t)
+	return int64(left)
 }
 
-func lcm(x, y int) int {
-	return x * y / gcd(x, y)
-}
-
-func gcd(x, y int) int {
-	tmp := x % y
-	if tmp > 0 {
-		return gcd(y, tmp)
-	} else {
-		return y
+func calcTrips(t int, time []int) int {
+	count := 0
+	for i := 0; i < len(time); i++ {
+		count += t / time[i]
 	}
+
+	return count
 }
