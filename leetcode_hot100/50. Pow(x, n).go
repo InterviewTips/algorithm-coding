a8b78a03e5c538d65357a173596129a71488a0c6
ashing <axingfly@gmail.com>
2022-03-12 15:04:49 +0800

    [feat] 50. Pow(x, n)

--- leetcode_hot100/50. Pow(x, n).go
+++ leetcode_hot100/50. Pow(x, n).go
@@ -1,5 +1,38 @@
 package leetcode_hot100
 
-func myPow(x float64, n int) float64 {
+func myPow(a float64, b int) float64 {
+
+	var subMyPow func(x float64, n int) float64
+	subMyPow = func(x float64, n int) float64 {
+		if n == 0 {
+			return 1
+		}
+
+		if n == 1 {
+			return x
+		}
+
+		result := subMyPow(x, n/2)
+		result *= result
+
+		if n&1 == 1 {
+			result *= x
+		}
+
+		return result
+	}
+
+	flag := false
+	if b < 0 {
+		flag = true
+		b = -b
+	}
+
+	res := subMyPow(a, b)
+	if !flag {
+		return res
+	}
+
+	return 1 / res
 
 }
