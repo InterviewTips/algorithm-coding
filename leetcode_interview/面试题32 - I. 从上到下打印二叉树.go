71fdb5e1c9d4c5694f5a3f22cbffe843a0e9b920
ashing <axingfly@gmail.com>
2022-03-18 22:59:12 +0800

    [feat] 面试题32 - I. 从上到下打印二叉树

---
+++ leetcode_interview/面试题32 - I. 从上到下打印二叉树.go
@@ -0,0 +1,32 @@
+package leetcode_interview
+
+import "github.com/InterviewTips/algorithm-coding/topic/binary_tree"
+
+type TreeNode = binary_tree.TreeNode
+
+func levelOrder(root *TreeNode) []int {
+	// 层次遍历
+	// 使用队列
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) != 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			res = append(res, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return res
+}
