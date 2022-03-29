51077db9b4efbbf1efdd64c6e58dc00b13d895ad
ashing <axingfly@gmail.com>
2022-03-29 23:38:33 +0800

    [feat] 2024. Maximize the Confusion of an Exam

---
+++ leetcode_daily/20220329/2024. Maximize the Confusion of an Exam_test.go
@@ -0,0 +1,31 @@
+package _0220329
+
+import "testing"
+
+func Test_maxConsecutiveAnswers(t *testing.T) {
+	type args struct {
+		answerKey string
+		k         int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "one",
+			args: args{
+				answerKey: "TTFF",
+				k:         2,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxConsecutiveAnswers(tt.args.answerKey, tt.args.k); got != tt.want {
+				t.Errorf("maxConsecutiveAnswers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
