f407c5df35456bf5f6743a032b11c763e67fee1f
ashing <axingfly@gmail.com>
2023-01-19 14:42:25 +0800

    feat: Remove Duplicate Node LCCI

---
+++ leetcode_interview/面试题 02.01. 移除重复节点.go
@@ -0,0 +1,27 @@
+package leetcode_interview
+
+import "algorithm/guns"
+
+type ListNode = guns.ListNode
+
+func removeDuplicateNodes(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	data := make(map[int]struct{})
+	data[head.Val] = struct{}{}
+	pos := head
+	for pos.Next != nil {
+		cur := pos.Next
+		_, ok := data[cur.Val]
+		if !ok {
+			pos = pos.Next
+			data[cur.Val] = struct{}{}
+			continue
+		}
+		// exists
+		pos.Next = pos.Next.Next
+	}
+
+	return head
+}
