aa1ee69b52050f6367a5a9d2b3d7349ea84c9f3d
ashing <axingfly@gmail.com>
2023-02-12 01:09:08 +0800

    feat: 217. Contains Duplicate

---
+++ leetcode/0217/217. Contains Duplicate_test.go
@@ -0,0 +1,36 @@
+package _217
+
+import "testing"
+
+func Test_containsDuplicate(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "one",
+			args: args{
+				nums: []int{1, 2, 3, 1},
+			},
+			want: true,
+		},
+		{
+			name: "two",
+			args: args{
+				nums: []int{1, 2, 3, 4},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := containsDuplicate(tt.args.nums); got != tt.want {
+				t.Errorf("containsDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
