7e8a78405b3f4ae1cfa54e681e7dedb8b6d215e7
ashing <axingfly@gmail.com>
2024-02-04 00:37:09 +0800

    fix: r
    Signed-off-by: ashing <axingfly@gmail.com>

--- leetcode/1619/1619. Mean of Array After Removing Some Elements.go
+++ leetcode/1619/1619. Mean of Array After Removing Some Elements.go
@@ -1,1 +1,13 @@
 package _619
+
+import "sort"
+
+func trimMean(arr []int) float64 {
+	sort.Ints(arr)
+	n := len(arr)
+	sum := 0
+	for _, x := range arr[n/20 : 19*n/20] {
+		sum += x
+	}
+	return float64(sum*10) / float64(n*9)
+}
