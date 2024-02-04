104e8af810c9e6a36a7b9a054342717e28bb9d6a
ashing <axingfly@gmail.com>
2024-02-04 10:08:18 +0800

    feat: 292. Nim Game
    Signed-off-by: ashing <axingfly@gmail.com>

---
+++ leetcode/0292/292. Nim Game_test.go
@@ -0,0 +1,36 @@
+package _292
+
+import "testing"
+
+func Test_canWinNim(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "one",
+			args: args{
+				n: 4,
+			},
+			want: false,
+		},
+		{
+			name: "two",
+			args: args{
+				n: 2,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canWinNim(tt.args.n); got != tt.want {
+				t.Errorf("canWinNim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
